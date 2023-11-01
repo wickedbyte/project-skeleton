21cff14b879a74be12f188f1194dff3c6b96a629
Andy Snell <andy@wickedbyte.com>
2023-11-01 14:42:26 -0500

    Update PsySH and PHP INI Settings

---
+++ .psysh.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'commands' => [],
+    'defaultIncludes' => [],
+    'eraseDuplicates' => true,
+    'errorLoggingLevel' => \E_ALL,
+    'forceArrayIndexes' => false,
+    'pager' => 'less',
+    'useBracketedPaste' => true,
+    'verbosity' => \Psy\Configuration::VERBOSITY_NORMAL,
+];
